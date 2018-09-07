2ba3970fc0689ee1862eef066a16e47d74470943
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-09-06 19:19:50 -0700

    start new component

---
+++ src/AddItem.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+export default class IApp extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h2>Hello!</h2>
+      </div>
+    );
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
+import AddItem from './AddItem';
 import './App.css';
 
-import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Do all the things.</h1>
+          <AddItem/>
+          
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
       </div>
     );
   }
